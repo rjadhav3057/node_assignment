4aae039565ef89b6714c1d38df960b142651911a
rjadhav3057 <rjadhav3057@gmail.com>
2019-12-05 11:13:30 +0530

    assignment1

---
+++ db_connection.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var cors = require('cors')
+var app = express();
+
+app.use(cors());
+app.use( bodyParser.json() );       
+app.use(bodyParser.urlencoded({     
+  extended: true
+}));
+
+
+app.listen(3000);
+
+var con = mysql.createConnection ({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'studentdetails'
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM stud", function (err, result, fields) {
+    if (err) throw err;
+    });
+ });
+
+ app.get('/student', function (req, res) {
+    con.query('select * from stud', function (error, results) {
+       if (error) throw error;
+       res.end(JSON.stringify(results));
+     });
+ });
+
+ app.post('/student/save', function (req, res) {
+    var postData = req.body;
+    console.log(postData);
+    con.query("INSERT INTO stud (`fname`,`lname`) VALUES ('"+postData.fname+"','"+postData.lname+"');", 
+    postData, function (error, results, fields) {
+      if (error) throw error;
+      res.end(JSON.stringify(results));
+    });
+  });
+
+ app.get('/student1', function (req, res) {
+  var id = req.param('id');
+  con.query('select * from stud where id=?',[id], function (error, results,) {
+     if (error) throw error;
+     res.end(JSON.stringify(results));
+     console.log(results);
+   });
+});
+
---
+++ main.js
@@ -0,0 +1,78 @@
+function savedata()
+{
+var first=document.getElementById("firstname").value;
+var last=document.getElementById("lastname").value;
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+  }
+};
+xmlhttp.open("POST", "http://localhost:3000/student/save", true);
+xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+xmlhttp.send(JSON.stringify({ "fname": first,"lname": last, "response": { "fname": first,"lname":last } }));
+}
+
+
+function stud_preview()
+{
+  var id=Number(document.getElementById("stud_id").value);
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var a=JSON.parse(xhttp.response);
+      console.log(a);
+      if(a.length > 0){
+      document.getElementById("stud_fname").value=a[0].fname;
+      document.getElementById("stud_lname").value=a[0].lname;
+      }
+    }
+  };
+  xhttp.open("GET", "http://localhost:3000/student1?id="+id, false);
+  xhttp.setRequestHeader("Content-Type", "application/json");
+  xhttp.send();
+}
+
+function addTable()
+{
+  
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var a=JSON.parse(xhttp.response);
+
+      console.log(a);
+
+  var myTableDiv = document.getElementById("myDynamicTable");
+    
+  var table = document.createElement('TABLE');
+  table.border='1';
+  
+  var tableBody = document.createElement('TBODY');
+  table.appendChild(tableBody);
+    
+  for (var i=0; i<a.length; i++){
+     var tr = document.createElement('TR');
+     tableBody.appendChild(tr);
+         var tdId = document.createElement('TD');
+         tdId.width='75';
+         console.log( a[i].id);
+         tdId.appendChild(document.createTextNode( a[i].id));
+         var tdFName = document.createElement('TD');
+         tdFName.width='75';
+         tdFName.appendChild(document.createTextNode(a[i].fname));
+         var tdLName = document.createElement('TD');
+         tdLName.width='75';
+         tdLName.appendChild(document.createTextNode( a[i].lname));
+         tr.appendChild(tdId);
+         tr.appendChild(tdFName);
+         tr.appendChild(tdLName);
+  }
+  myTableDiv.appendChild(table);
+
+    }
+  };
+  xhttp.open("GET", "http://localhost:3000/student",true);
+  xhttp.setRequestHeader("Content-Type", "application/json");
+  xhttp.send();
+}
+
